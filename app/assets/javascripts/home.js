81cffb893208896b430fe4e53def365b29d0a961
Julien Pellet <pelletj@gmail.com>
2011-12-13 11:37:59 +1300

    add CSS for invalid field

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -33,16 +33,21 @@ function checkNewCandidateFied(event){
 
 	/// Check if name exists
 			
-			event.target.set('class', 'inputText-validated');
+			event.target.set('class', 'inputText-valid');
 
 			break;
 		case "candidateUrl":
 
 
 	/// Validate URL
-
-
-			event.target.set('class', 'inputText-validated');
+			if (validateURL(event.target.value)) 
+			{
+				event.target.set('class', 'inputText-valid');
+			}
+			else
+			{
+				event.target.set('class', 'inputText-invalid');
+			}
 			break;
 
 	}
@@ -70,6 +75,11 @@ function addCandidate(){
 
 }
 
+ function validateURL(textval) {
+      var urlregex = new RegExp(
+            "^(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*$");
+      return urlregex.test(textval);
+    }
 
 
 function addUrl(){
