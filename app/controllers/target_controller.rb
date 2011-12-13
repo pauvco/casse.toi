ed4eb0c5fec7ce773bf9ad051687f1b4c052d745
Julien Pellet <pelletj@gmail.com>
2011-12-13 14:59:56 +1300

    intellingent code ...

--- app/controllers/target_controller.rb
+++ app/controllers/target_controller.rb
@@ -2,9 +2,7 @@ class TargetController < ApplicationController
   def show
 	@target = Target.find_by_name(params[:name])
 
-	if @target.validated || user_signed_in?
-puts "OK"
-	else
+	if !@target.validated && !user_signed_in?
 		redirect_to url_for(:controller => 'home', :action => 'index')
 	end
   end
